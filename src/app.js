a1e94d1b61ad4f0fb8c4360e84587aa3250de2ca
Supun Thilanka <e19349@eng.pdn.ac.lk>
2024-08-11 19:53:17 +0530

    backend port

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use('/api', towerRoutes);
 app.use('/api', equipmentRoutes);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
