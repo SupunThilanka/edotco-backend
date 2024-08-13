f24c59ecfd6108d90d230c315cf246ccef8d2f01
Supun Thilanka <e19349@eng.pdn.ac.lk>
2024-08-13 16:11:26 +0530

    add kafka API

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // Import the cors package
 const towerRoutes = require('./routes/towerRoutes');
-const equipmentRoutes = require('./routes/equipmentRoutes');
+const equipmentRoutes = require('./routes/equipmentRoutes');  
+const kafkaRoutes = require('./routes/kafkaRoutes');  
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 
 app.use('/api', towerRoutes);
 app.use('/api', equipmentRoutes);
+app.use('/api', kafkaRoutes);
 
 const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => {
--- src/routes/towerRoutes.js
+++ src/routes/towerRoutes.js
@@ -5,13 +5,10 @@ const towerController = require('../controllers/towerController');
 router.get('/towers', towerController.getAllTowers);
 router.get('/tower-types', towerController.getTowerTypes);
 router.post('/towers', towerController.createTower);
-
 // Add the route to get a single tower by its ID
 router.get('/towers/:id', towerController.getTowerById);
-
 // Add the update route
 router.put('/towers/:id', towerController.updateTower);
-
 // Add the delete route
 router.delete('/towers/:id', towerController.deleteTower);
 
